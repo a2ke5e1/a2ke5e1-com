19179da4b5cc8a1901cbf5db8e01abeb01b893f3
a2ke5e1 <aakapurv@gmail.com>
2024-10-12 15:38:39 +0530

    Refactor image rendering code and apply consistent blur effect

--- src/app/renders/page.tsx
+++ src/app/renders/page.tsx
@@ -1,3 +1,109 @@
-export default function Renders() {
-    return <div className="text-headline-large">renders</div>
-}
+
+
+import { Metadata, NextPage } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Renders | a2ke5e1.com",
+  description: "Artwork and Renders by Apurv Ajay Kumar",
+};
+
+const RenderPage: NextPage = () => {
+  return (
+    <>
+      <div className="mb-8 flex flex-col gap-4">
+        <div className="bg-tertiary-container p-14 rounded-xl flex gap-1 flex-col justify-center items-center">
+          <h1 className="text-display-large font-bold">{"Renders & Artworks"}</h1>
+          <p className="text-title-small text-on-tertiary-container">
+            Some wallpapers, logos and other artwork I have created while learning Blender
+          </p>
+        </div>
+        <div className="grid grid-flow-row md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2">
+          {artwork.map((item) => (
+            <Image
+              key={item.img}
+              src={item.img}
+              alt={item.title}
+              loading="lazy"
+              width={164}
+              height={164}
+              layout="responsive"
+              className="rounded-2xl"
+              placeholder="blur"
+              blurDataURL={item.blur}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+export default RenderPage;
+
+const artwork = [
+  {
+    img: "/images/renders/test4.png",
+    title: "Donout with Tea",
+    blur: "/images/renders/blur/test4.jpg",
+  },
+  {
+    img: "/images/renders/donout3.png",
+    title: "Donout 2",
+    blur: "/images/renders/blur/donout3.jpg",
+  },
+  {
+    img: "/images/renders/donout2.png",
+    title: "Donout with Utils",
+    blur: "/images/renders/blur/donout2.jpg",
+  },
+  {
+    img: "/images/renders/donout.png",
+    title: "Donout",
+    blur: "/images/renders/blur/donout.jpg",
+  },
+  {
+    img: "/images/renders/background.png",
+    title: "Baxkground Artwork",
+    blur: "/images/renders/blur/background.jpg",
+  },
+  {
+    img: "/images/renders/glasses.png",
+    title: "Glasses",
+    blur: "/images/renders/blur/glasses.jpg",
+  },
+  {
+    img: "/images/renders/dagger.png",
+    title: "Dagger",
+    blur: "/images/renders/blur/dagger.jpg",
+  },
+  {
+    img: "/images/renders/knife.png",
+    title: "Dagger 2",
+    blur: "/images/renders/blur/knife.jpg",
+  },
+  {
+    img: "/images/renders/ice_cream.jpg",
+    title: "Ice Cream",
+    blur: "/images/renders/blur/ice_cream.jpg",
+  },
+  {
+    img: "/images/renders/image0005.png",
+    title: "Logo V2",
+    blur: "/images/renders/blur/image0005.jpg",
+  },
+  {
+    img: "/images/renders/logo_wall.png",
+    title: "Logo",
+    blur: "/images/renders/blur/logo_wall.jpg",
+  },
+  {
+    img: "/images/renders/room.jpg",
+    title: "My Unfinised Room",
+    blur: "/images/renders/blur/room.jpg",
+  },
+  {
+    img: "/images/renders/phycalc3.png",
+    title: "Physics Calculator Logo with Style",
+    blur: "/images/renders/blur/phycalc3.jpg",
+  },
+];
