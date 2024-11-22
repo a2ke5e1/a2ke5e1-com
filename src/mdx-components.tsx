d65269dd1a4a6c07e9f502e5b2daef9136113e6c
a2ke5e1 <aakapurv@gmail.com>
2024-11-22 14:09:02 +0530

    Add blockquote support and update h4 styling in MDX components; format blog entries

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -11,6 +11,7 @@ type CustomMdxComponents = {
   ul: ComponentType;
   ol: ComponentType;
   pre: ComponentType;
+  blockquote: ComponentType;
 };
 
 export const customMdxComponents: CustomMdxComponents = {
@@ -19,7 +20,7 @@ export const customMdxComponents: CustomMdxComponents = {
     <h2 className="text-display-medium font-bold underline " {...props} />
   ),
   h3: (props) => <h3 className="text-display-small" {...props} />,
-  h4: (props) => <h4 className="text-label-small text-primary" {...props} />,
+  h4: (props) => <h4 className="text-label-small text-tertiary" {...props} />,
   p: (props) => <p className="text-body-large" {...props} />,
   a: (props) => (
     <a className="text-primary underline text-body-large" {...props} />
@@ -33,6 +34,12 @@ export const customMdxComponents: CustomMdxComponents = {
   pre: (props) => (
     <pre
       className="text-body-large text-on-surface-variant bg-surface-container p-4 rounded-lg"
+      {...props}
+    />
+  ),
+  blockquote: (props) => (
+    <blockquote
+      className="text-body-large bg-primary-container text-on-primary-container p-4 rounded-lg"
       {...props}
     />
   ),
